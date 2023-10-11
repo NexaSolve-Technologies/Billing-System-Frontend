a1d0f813167f95b27af2a3fc9907806cda8cb711
darshan5203 <dsojitra03@gmail.com>
2023-10-12 05:14:52 +0530

    new package folder created

---
+++ src/components/newpackage/newpackage.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import Navbar from '../navbar/navbar';
+import './newpackage.css';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+
+function NewPackage(){
+
+    return(
+        <div>
+             <div>
+                <nav className='navb' >
+                <div>Your Logo</div>
+                </nav>
+            </div>
+            
+        </div>
+    )
+
+}
+export default NewPackage;
