f7e76148a16a67410970c23e2d66f9346aa4cc6e
AbhishekBharati <210305105048@paruluniversity.ac.in>
2023-10-14 13:07:47 +0530

    Added Deletion Page

---
+++ src/pages/DeletePackages/deletepackage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DeletePackage () {
+
+    return (
+        <div>
+            Delete Package
+        </div>
+    )
+}
+
+export default DeletePackage;
---
+++ src/pages/DeletePackages/deletepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DeleteProduct from "./deleteproduct";
+import DeletePackage from "./deletepackage";
+
+function DeletePage() {
+    return (
+        <div>
+            <DeleteProduct />
+            {/* <DeletePackage /> */}
+        </div>
+    )
+}
+
+export default DeletePage;
